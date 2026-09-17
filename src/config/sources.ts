2d21d316efddac4ce65f9af2979639a9cd65888e
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-09-17 12:35:32 +0900

    feat: define ranking data sources

---
+++ src/config/sources.ts
@@ -0,0 +1,10 @@
+export const RANKING_SOURCES = {
+  official: {
+    listUrl: "https://w.atwiki.jp/f_go/pages/671.html",
+  },
+  original: {
+    listUrl: "https://w.atwiki.jp/siroi_human/pages/54.html",
+    sectionHeading: "サーヴァント",
+    ignoredSectionHeading: "データ",
+  },
+} as const;
